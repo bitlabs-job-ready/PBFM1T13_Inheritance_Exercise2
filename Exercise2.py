04589e5db3720c093aa5e9f5207ad1e962df3375
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-05-10 18:16:00 +0530

    Add files via upload

---
+++ Exercise2.py
@@ -0,0 +1,47 @@
+
+     Create an abstract class Shape with following data member and methods- 
+     Create data members for height and width.
+     Create constructor and assign local varaible values to instance varaibles 
+     Create methods for finding area and perimeter.
+     Create three subclasses Square, Rectangle and EquilateralTriangle that extends Shape class and define both the methods.
+     Write a program that will find the area and perimeter of 3 Shapes and print the details for all. 
+     Prompt the user for the  values to be input from the terminal.
+ 
+     Sample Input:
+     Enter Width of Rectangle in meters
+     10
+     Enter Length of Rectangle in meters
+     5
+     Enter width of Equilateraltriangle
+     15
+     Enter radius of circle
+     60
+     Expected Output:
+     Rectangle width: 10.0 meters and length: 5.0 meters
+     Resulting area: 50.0 square meters
+     Resulting perimeter: 30.0 meters 
+     EquiTriangle side: 15.0meters
+     Resulting area: 97 square meters
+     Resulting perimeter: 45.0 meters 
+     Circle radius : 60.0meters
+     Resulting area: 11310 square meters
+     Resulting perimeter: 377 meters  
+
+
+class Shape: 
+        // declare height and width variables
+       // create construtor and Assign height and width to instance varaibles
+       // create a methods to print height and width
+  
+//Add Square class that extends Shape class
+class Square:
+       // add methods to find area and perimeter of Square
+
+//Add Rectangle class that extends Shape class
+class Rectangle:
+            // add methods to find area and perimeter of Rectangle
+
+//Add EquilateralTriangle class that extends Shape class
+class EquilateralTriangle
+            // add methods to find area and perimeter of EquilateralTriangle     
+
